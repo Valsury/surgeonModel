f45b4ba13cb9cdf5c8107dbec187c29a9e04e20f
Valsury <valsury@mail.ru>
2024-12-26 17:59:47 +0300

    Initial commit

---
+++ src/main/java/com/example/surgeonmodel/SurgeonModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.surgeonmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurgeonModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurgeonModelApplication.class, args);
+    }
+
+}
